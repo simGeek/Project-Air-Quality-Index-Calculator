0f7c57bc4aed29ba5a55d129daa85c59c2762af4
Simran <simran.96@outlook.in>
2025-03-05 13:47:53 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,5 +3,5 @@ from home import views
 
 urlpatterns = [
     path('', views.aqi_pollutants, name='aqi_pollutants'),
-    path("aqi_results", views.aqi_results, name='aqi_results')
+    path("aqi_regression", views.aqi_regression, name='aqi_regression')
 ]
